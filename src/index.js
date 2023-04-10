8c830391585875e3ff239ed87d6a7ceeebc109ca
turanarican <turanarican1985@gmail.com>
2023-04-10 14:01:59 +0300

    commit on video7

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const App = () => (
-  <div>
-    <h1>AdCupy</h1>
-  </div>
+  <>
+    <h1 className="Logo">AdCupy</h1>
+    <h2>it will success</h2>
+  </>
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
