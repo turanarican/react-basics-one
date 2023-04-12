cc91cfe198441818338529e21e406ab1d0c284d1
turanarican <turanarican1985@gmail.com>
2023-04-12 20:36:09 +0300

    commit on video12 v1

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
 
+const getInputValue = (e) => {
+  console.log(e.target.value);
+};
+
 class Header extends Component {
   render() {
     return (
       <>
         <header>
-          <h1 className="logo">Logo</h1>
-          <input />
+          <h1 className="logo" onClick={() => console.log("logo clicked")}>
+            Logo
+          </h1>
+          <input onChange={getInputValue} />
         </header>
       </>
     );
