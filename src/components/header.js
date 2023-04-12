027493f186cf3ce8c690a71d099a18bc4e2c1331
turanarican <turanarican1985@gmail.com>
2023-04-12 23:57:49 +0300

    no change

--- src/components/header.js
+++ src/components/header.js
@@ -1,50 +1,43 @@
-import React,{ Component } from "react";
+import React, { Component } from "react";
 
 class Header extends Component {
+  state = {
+    title: "The keyword are:",
+    keywords: "",
+    count: 0,
+  };
 
-    state = {
-        title:'The keyword are:',
-        keywords:'',
-        count:0
-    }
+  inputChange = (event) => {
+    this.setState({
+      keywords: event.target.value,
+    });
+  };
 
-    
-    inputChange = (event) => {
-        this.setState({
-            keywords: event.target.value
-        })
-    }
+  addOne() {
+    this.setState((state, props) => ({
+      count: state.count + 1,
+    }));
+  }
 
-    addOne(){
-        this.setState((state,props)=>({
-            count: state.count + 1
-        }))
-    }
-
-
-    render(){
-        //console.log(this.state)
-        return (
-            <>
-                <header>
-                    <div 
-                        className="logo"
-                        onClick={()=> console.log('I was clicked')}
-                    >Logo</div>
-                    <input
-                        onChange={(e)=> this.inputChange(e)}
-                    />
-                    <br/>
-                    <div>{this.state.title}</div>
-                    <div>{this.state.keywords}</div>
-                    <br/>
-                    <div>{this.state.count}</div>
-                    <button onClick={()=>this.addOne()}>Add one</button>
-                </header>
-            </>
-        )
-    }
-
+  render() {
+    //console.log(this.state)
+    return (
+      <>
+        <header>
+          <div className="logo" onClick={() => console.log("I was clicked")}>
+            Logo
+          </div>
+          <input onChange={(e) => this.inputChange(e)} />
+          <br />
+          <div>{this.state.title}</div>
+          <div>{this.state.keywords}</div>
+          <br />
+          <div>{this.state.count}</div>
+          <button onClick={() => this.addOne()}>Add one</button>
+        </header>
+      </>
+    );
+  }
 }
 
 export default Header;
