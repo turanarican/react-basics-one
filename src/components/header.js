7d596bee82814d1a31184084c1b2c45613ab3868
turanarican <turanarican1985@gmail.com>
2023-04-12 23:53:39 +0300

    cımmit on video13

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
 
 class Header extends Component {
-  getInputValue = (e) => {
-    console.log(e.target.value);
+  state = {
+    text: "",
   };
+
+  changeText(e) {
+    this.setState({ text: e.target.value });
+  }
+
   render() {
     return (
       <>
@@ -11,7 +16,8 @@ class Header extends Component {
           <h1 className="logo" onClick={() => console.log("logo clicked")}>
             Logo
           </h1>
-          <input onChange={(e) => this.getInputValue(e)} />
+          <input onChange={(e) => this.changeText(e)} />
+          <h2>{this.state.text}</h2>
         </header>
       </>
     );
