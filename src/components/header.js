456a6bcd417a69606da0a091b6199c007227373f
turanarican <turanarican1985@gmail.com>
2023-04-13 00:03:51 +0300

    m

--- src/components/header.js
+++ src/components/header.js
@@ -3,12 +3,15 @@ import React, { Component } from "react";
 class Header extends Component {
   state = {
     text: "",
+    count: 0,
   };
 
   changeText(e) {
     this.setState({ text: e.target.value });
   }
-
+  addToCounter() {
+    this.setState((state) => ({}));
+  }
   render() {
     return (
       <>
@@ -18,6 +21,9 @@ class Header extends Component {
           </h1>
           <input onChange={(e) => this.changeText(e)} />
           <h2>{this.state.text}</h2>
+          <br />
+          <h3>this.state.count</h3>
+          <button onClick={this.addToCounter}>Add</button>
         </header>
       </>
     );
