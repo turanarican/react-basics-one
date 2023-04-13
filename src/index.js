cdcd2ed96f432b8b882f8b93b97e0204c5137006
turanarican <turanarican1985@gmail.com>
2023-04-13 21:19:09 +0300

    commit on video16

--- src/index.js
+++ src/index.js
@@ -7,14 +7,16 @@ import JSON from "./db.json";
 
 import Header from "./components/header.js";
 import NewsList from "./components/news_list";
+import Footer from "./components/footer";
 
 class App extends Component {
-  state = { news: JSON };
+  state = { news: JSON, footerText: "some footer text" };
   render() {
     return (
       <>
         <Header />
         <NewsList news={this.state.news} />
+        <Footer footerText={this.state.footerText} />
       </>
     );
   }
