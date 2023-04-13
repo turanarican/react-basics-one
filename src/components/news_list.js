81198318b366e30ffb42e81f8047f15c2e617fb3
turanarican <turanarican1985@gmail.com>
2023-04-13 18:20:40 +0300

    commit exercise between video14 and video15

--- src/components/news_list.js
+++ src/components/news_list.js
@@ -2,9 +2,7 @@ import NewsListItem from "./news_list_item";
 
 const NewsList = ({ news }) => {
   console.log(news);
-  return news.map((item) => {
-    return <NewsListItem item={item} key={item.id} />;
-  });
+  return news.map((item) => <NewsListItem item={item} key={item.id} />);
 };
 
 export default NewsList;
